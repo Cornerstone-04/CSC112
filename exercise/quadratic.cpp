837cce2590e1b03149a81cad3a7054ee2c7d73e7
Cornerstone Ephraim <fortunecornerstone@gmail.com>
2023-07-16 17:33:29 +0100

    file structure fixed

--- exercise/quadratic.cpp
+++ exercise/quadratic.cpp
@@ -46,3 +46,21 @@ int main()
 
     return 0;
 }
+
+/**
+  ## Pseudo Code
+
+1. Start.
+2. Input a, b, c.
+3. Let d = (b * b) - (4 * a * c).
+4. if(desc > 0)
+    <br> 4.1 x1 = (-b + sqrt(desc))/(2 * a)
+    <br> 4.2 x2 = (-b - sqrt(desc))/(2 * a)
+    <br> 4.3 output x1, x2.
+5. if(desc = 0)
+    <br> 5.1 x1 = x2 = -b/(2 * a).
+    <br> 5.2 output x1 = x2 = x.
+6. if(desc < 0)
+    <br> 6.1 output "Complex roots".
+7. Stop.
+*/
