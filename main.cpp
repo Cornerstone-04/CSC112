fb59460e28b333b3c09edd6dfc9de83f3301f1e6
Cornerstone Ephraim <fortunecornerstone@gmail.com>
2023-07-27 06:25:53 +0100

    notes for Tolu

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // string name;
+    // cout << "enter name: ";
+    // cin >> name;
+    // cout << name;
+
+    // constant
+    const string name = "Tolu";
+    int age = 10;
+
+    age = name == "Barry" ? 18 : 19;
+
+    cout << age;
+    /**
+     * bool check; //declaration
+        check = true; //initialisation
+    */
+
+    bool check = true;
+
+    return 0;
+}
